a5c340bf57737267672111a1c883eb886d551603
Daniel <daniel.binder@eyp.at>
2019-02-18 15:50:22 +0100

    Add version.php

---
+++ version.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel
+ * Date: 18/02/2019
+ * Time: 15:36
+ */
+
+$plugin->component = 'tool_daniel';
+$plugin->version = 2019021800;
+$plugin->requires = 2014051200; // Moodle 2.7.0 is required.
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release ='v1.0';
